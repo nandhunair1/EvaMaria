4c330a0d6f4aa22bc31d302d65965b005914463d
Venom <67759177+nandhuachu@users.noreply.github.com>
2021-10-30 19:48:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -351,12 +351,18 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 f_caption=f_caption
         if f_caption is None:
             f_caption = f"{title}"
-                await query.answer()
-                await client.send_cached_media(
-                    chat_id=query.from_user.id,
-                    file_id=file_id,
-                    caption=f_caption
-                    )
+        buttons = [
+            [
+                InlineKeyboardButton("♻️ ⒼⓇⓄⓊⓅ ♻️", url="https://t.me/tvseriezzz")
+            ]
+            ]
+
+         await query.answer()
+         await client.send_cached_media(
+             chat_id=query.from_user.id,
+             file_id=file_id,
+             caption=f_caption
+             )
 
     elif query.data == "pages":
         await query.answer()
