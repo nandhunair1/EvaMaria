c3823a151ac3e66547ea0b6f501d23d5482281b8
Venom <67759177+nandhuachu@users.noreply.github.com>
2021-10-30 13:30:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -29,7 +29,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
-            photo=random.choice(PICS),
+            video=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention),
             reply_markup=reply_markup,
             parse_mode='html'
