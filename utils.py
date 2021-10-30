0054cafc0b160bdaf669df43d15f6f68bb14c90c
Venom <67759177+nandhuachu@users.noreply.github.com>
2021-10-30 12:19:58 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -303,7 +303,7 @@ def humanbytes(size):
         return ""
     power = 2**10
     n = 0
-    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    Dic_powerN = {0: ' ', 1: 'Kb', 2: 'Mb', 3: 'Gb', 4: 'Tb'}
     while size > power:
         size /= power
         n += 1
