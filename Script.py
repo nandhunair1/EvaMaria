9e4ed96a55ade34150dc43ae89a525e2d0b265ab
Venom <67759177+nandhuachu@users.noreply.github.com>
2021-10-31 15:49:32 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -44,7 +44,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https//t.me/tvseriezzz_bot)</code>
+<code>[Button Text](buttonurl:https://t.me/tvseriezzz_bot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
@@ -52,7 +52,7 @@ class script(object):
 
 <b>NOTE:</b>
 1. Make me the admin of your channel if it's private.
-2. make sure that your channel does not contains cam rip, porn and fake files.
+2. make sure that your channel does not contains camrip, porn and fake files.
 3. Forward the last message to me with quotes.
  I'll add all the files in that channel to my db."""
     CONNECTION_TXT = """Help: <b>Connections</b>
