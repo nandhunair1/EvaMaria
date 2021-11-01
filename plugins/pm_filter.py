ec0913e54a6e2d21ebce5d87befec41e2382ecef
Venom <67759177+nandhuachu@users.noreply.github.com>
2021-11-01 16:25:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -354,7 +354,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 f_caption=f_caption
         if f_caption is None:
             f_caption = f"{title}"
-
         await query.answer()
         await client.send_cached_media(
             chat_id=query.from_user.id,
