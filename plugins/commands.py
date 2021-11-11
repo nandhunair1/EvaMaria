096a7efa57cef8a740fb2a8bb74fe2ff1663fd06
Venom <67759177+nandhuachu@users.noreply.github.com>
2021-11-11 13:52:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,7 +6,7 @@ from Script import script
 from pyrogram import Client, filters
 from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from database.ia_filterdb import Media, get_file_details
+from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from database.users_chats_db import db
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, LOG_CHANNEL, PICS
 from utils import get_size, is_subscribed, temp
@@ -176,6 +176,8 @@ async def delete(bot, message):
         await message.reply('Reply to file with /delete which you want to delete', quote=True)
         return
 
+    file_id, file_ref = unpack_new_file_id(media.file_id)
+
     for file_type in ("document", "video", "audio"):
         media = getattr(reply, file_type, None)
         if media is not None:
@@ -188,6 +190,7 @@ async def delete(bot, message):
         'file_name': media.file_name,
         'file_size': media.file_size,
         'mime_type': media.mime_type
+        '_id': file_id
     })
     if result.deleted_count:
         await msg.edit('File is successfully deleted from database')
