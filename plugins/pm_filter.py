ffbf78781f8a018fcc5eb42df7e38bc6f212feb7
Venom <67759177+nandhuachu@users.noreply.github.com>
2021-11-20 21:43:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -156,7 +156,7 @@ async def next_page(bot, query):
         n = await query.edit_message_reply_markup( 
             reply_markup=InlineKeyboardMarkup(btn)
         )
-        await asyncio.sleep(8)
+        await asyncio.sleep(600)
         await n.delete()
     except MessageNotModified:
         pass
@@ -721,22 +721,22 @@ async def auto_filter(client, msg, spoll=False):
     if imdb and imdb.get('poster'):
         try:
             b = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(8)
+            await asyncio.sleep(600)
             await b.delete()
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
             await message.reply_photo(photo=poster, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            c = await asyncio.sleep(8)
+            c = await asyncio.sleep(600)
             await c.delete()
         except Exception as e:
             logger.exception(e)
             await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-            a = await asyncio.sleep(8)
+            a = await asyncio.sleep(600)
             await a.delete()
     else:
         d = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-        await asyncio.sleep(8)
+        await asyncio.sleep(600)
         await d.delete()
     if spoll:
         await msg.message.delete()
