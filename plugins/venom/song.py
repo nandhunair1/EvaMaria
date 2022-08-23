b0f7ee735bebe5ad2b83356eadf0894b40bebf41
Venom <67759177+nandhuachu@users.noreply.github.com>
2022-08-23 17:46:18 +0530

    Update song.py

--- plugins/venom/song.py
+++ plugins/venom/song.py
@@ -27,7 +27,7 @@ def time_to_seconds(time):
 
 ## Commands --------------------------------
 
-@Client.on_message(filters.command(['song']) & filters.group & filters.incoming)
+@Client.on_message(filters.command(['song']) & filters.incoming)
 def a(client, message):
     query = ''
     for i in message.command[1:]:
