b26d76c5cc585187f38ad8106004a0807512f3dc
Venom <67759177+nandhuachu@users.noreply.github.com>
2022-09-07 21:00:12 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -85,28 +85,6 @@ class script(object):
 <code>[Button Text](buttonalert:This is an alert message)</code>
 
 ©️ MᴀɪɴᴛᴀɪɴᴇD Bʏ : <a href='https://t.me/tvseriezzz_group'>♠️ 𝑨𝒍𝒍 𝑰𝒏 𝑶𝒏𝒆 𝑮𝒓𝒐𝒖𝒑 🎬</a>"""
-    SONG_TXT = """<b>🎼Song Download🎼</b>
-Song Download Module, For Those Who Love Music
-<b>🎈 Command 🎈</b>
-
-- /song [Song Name] - To Download Music 😁
-
-<b>🌀Usage🌀</b>
-
-- Can Be Used By Everyone
-
-- Works in bot pm
-
-©️ MᴀɪɴᴛᴀɪɴᴇD Bʏ : <a href='https://t.me/tvseriezzz_group'>♠️ 𝑨𝒍𝒍 𝑰𝒏 𝑶𝒏𝒆 𝑮𝒓𝒐𝒖𝒑 🎬</a>"""
-    VIDEO_TXT ="""<b>Help for Download any videos in YT</b>.
-• 𝘜𝘴𝘢𝘨𝘦
-𝘠𝘰𝘶 𝘊𝘢𝘯 𝘋𝘰𝘸𝘯𝘭𝘰𝘢𝘥 𝘈𝘯𝘺 𝘝𝘪𝘥𝘦𝘰 𝘍𝘳𝘰𝘮 𝘠𝘰𝘶𝘵𝘶𝘣𝘦
-𝙃𝙤𝙬 𝙏𝙤 𝙐𝙨𝙚
-• 𝘛𝘺𝘱𝘦 /video or /mp4 𝘈𝘯𝘥 (yt Link)
-• 𝘌𝘹𝘢𝘮𝘱𝘭𝘦:
-/𝘮𝘱4 yt Link
-
-©️ MᴀɪɴᴛᴀɪɴᴇD Bʏ : <a href='https://t.me/tvseriezzz_group'>♠️ 𝑨𝒍𝒍 𝑰𝒏 𝑶𝒏𝒆 𝑮𝒓𝒐𝒖𝒑 🎬</a>"""
     GTTSS_TXT = """Help: <b> TTS 🎤 module:</b>
 Translate text to speech
 
