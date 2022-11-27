e0014fef90cdcee665ecc4cd979506a1d5da2adf
Venom <67759177+nandhuachu@users.noreply.github.com>
2022-11-27 14:11:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/nandhuachu/aishu /EvaMaria
+  git clone https://github.com/nandhuachu/aishu /aishu
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /aishu
 fi
-cd /EvaMaria
+cd /aishu
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
