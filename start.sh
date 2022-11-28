ee5c893e1a78ce1c037cbbfba7ac7cf4a8d42253
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2022-11-29 01:05:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/nandhuachu/MrC_VENOM /EvaMaria
+  git clone https://github.com/MyBotzz/LUCIFER.git /LUCIFER
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /LUCIFER
 fi
-cd /EvaMaria
+cd /LUCIFER
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
